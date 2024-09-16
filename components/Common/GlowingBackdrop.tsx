ae8ebd660a2ded3e8ebc1f916ea0441a0ae58063
dannhan <ahmadanun1@gmail.com>
2024-09-16 15:16:45 +0700

    refactor: change css module to to tailwind classes

--- components/Common/GlowingBackdrop.tsx
+++ components/Common/GlowingBackdrop.tsx
@@ -1,9 +1,9 @@
 import type { FC } from "react";
 
-import styles from "./index.module.css";
-
 const GlowingBackdrop: FC = () => (
-  <div className={styles.glowingBackdrop}></div>
+  <div className="absolute -left-20 -top-20 -z-10 size-full opacity-50 md:opacity-100">
+    <div className="absolute inset-0 m-auto aspect-square h-full w-full rounded-full bg-[#406187] blur-[175px] sm:h-[448px] sm:w-[503px]"></div>
+  </div>
 );
 
 export default GlowingBackdrop;
