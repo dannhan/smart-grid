db77470357d4a6fa573c2f4d70e6e1aba4eeb228
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-11-01 01:50:58 +0700

    feat: implement mock stye for monitoring route

--- app/dashboard/monitoring/page.tsx
+++ app/dashboard/monitoring/page.tsx
@@ -1,45 +1,13 @@
-import Link from "next/link";
+import { ChartComponent } from "@/components/BarChart";
 
-import { ChevronRightIcon } from "lucide-react";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/shadcn/card";
-
-import { dashboardInfoItems } from "@/lib/config";
-
-const DashboardPage = () => {
+const MonitoringPage = () => {
   return (
-    <main className="flex flex-1 flex-col gap-8 px-4">
-      <div className="gap grid h-full w-full grid-cols-2 grid-rows-2 gap-x-12 gap-y-6">
-        {/* TODO: href */}
-        {dashboardInfoItems.map((item) => (
-          <Link href={"#"} key={item.title}>
-            <Card className="relative font-bold">
-              <CardHeader className="px-5 pb-0 pt-4">
-                <CardTitle className="text-2xl">{item.title}</CardTitle>
-                <ChevronRightIcon
-                  className="absolute right-4 top-3 size-5"
-                  strokeWidth={2.5}
-                />
-              </CardHeader>
-              <CardContent className="flex items-center justify-between px-5 pb-0 text-xl">
-                <div>{item.value}</div>
-                <div className="size-14">
-                  <item.icon />
-                </div>
-              </CardContent>
-              <CardFooter className="px-5 pb-5 text-sm">{item.unit}</CardFooter>
-            </Card>
-          </Link>
-        ))}
-      </div>
-      <Card className="h-full w-full" />
+    <main className="flex flex-1 flex-col gap-8 px-4 py-8">
+      <ChartComponent />
+      <ChartComponent />
+      <ChartComponent />
     </main>
   );
 };
 
-export default DashboardPage;
+export default MonitoringPage;
