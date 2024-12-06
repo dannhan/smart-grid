80f5124403c0bc0776214b4f32bc6d2fa6e8aec1
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2024-12-06 17:38:36 +0700

    feat: add seed for realtie database

--- actions/seedAction.ts
+++ actions/seedAction.ts
@@ -4,6 +4,7 @@ import seedComponents from "@/lib/seed/seedComponents";
 import seedHistory from "@/lib/seed/seedHistory";
 import seedRooms from "@/lib/seed/seedRooms";
 import seedUsers from "@/lib/seed/seedUsers";
+import seedRealtime from "@/lib/seed/seedRealtime";
 
 export async function seedAction() {
   if (process.env.NODE_ENV === "development") {
@@ -11,5 +12,6 @@ export async function seedAction() {
     await seedHistory();
     await seedRooms();
     await seedUsers();
+    await seedRealtime();
   }
 }
--- types/room.ts
+++ types/room.ts
@@ -3,8 +3,8 @@ import type { DocumentReference, Timestamp } from "firebase/firestore";
 // type for realtime database room
 export type ControlledRoom = {
   name: string;
-  lamp: number;
-  socket: number;
+  lamp: boolean;
+  socket: boolean;
 };
 
 // type for firestore room
